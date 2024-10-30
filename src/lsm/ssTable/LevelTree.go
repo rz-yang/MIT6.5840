7c6bb5afa2b8e59ad82b1830daa85fcb34e9946e
yang <502022330061@smail.nju.edu.cn>
2024-10-30 15:58:52 +0800

    lsmT - 实现level层级树结构

---
+++ src/lsm/ssTable/LevelTree.go
@@ -0,0 +1,56 @@
+package ssTable
+
+import (
+	"fmt"
+	"raft_LSMTree-based_KVStore/lsm/kv"
+	"sync"
+)
+
+// SSTable层次树
+type LevelTree struct {
+	levelsTail []*tableNode
+
+	mutex sync.RWMutex
+}
+
+// 每层的链表节点，指向每层的SSTable
+type tableNode struct {
+	table *SSTable
+	prev  *tableNode
+}
+
+// 从所有的SSTable查找数据
+func (tree *LevelTree) Search(key string) (kv.Value, kv.SearchResult) {
+	tree.mutex.RLock()
+	defer tree.mutex.RUnlock()
+	// 每一层遍历
+	for _, node := range tree.levelsTail {
+		for node != nil {
+			value, result := node.table.Search(key)
+			if result == kv.KeyNotFound {
+				node = node.prev
+			} else {
+				return value, result
+			}
+		}
+	}
+	return kv.Value{}, kv.KeyNotFound
+}
+
+func (tree *LevelTree) getCount(level int) int {
+	count := 0
+	ptr := tree.levelsTail[level]
+	for ptr != nil {
+		count++
+		ptr = ptr.prev
+	}
+	return count
+}
+
+func getLevel(name string) (level int, index int, err error) {
+	n, err := fmt.Sscanf(name, "%d.%d.db", &level, &index)
+	if n != 2 || err != nil {
+		return -1, -1, fmt.Errorf("incorrect data file name: %q", name)
+	}
+	return level, index, nil
+}
